801a8387531bc04afb4246eccee689239f50f84a
Iri O <oparina.iri@gmail.com>
2024-04-08 18:34:52 +0500

    Changed check_regex... func

--- apps/regex/services.py
+++ apps/regex/services.py
@@ -1,21 +1,18 @@
 import re
-from django.http import QueryDict
 
 
-class RegexMatchService:
-    @staticmethod
-    def check_regex_and_get_response(pattern: str, string: str):
-        response = 'Текст не найден'
-        match = None
+def check_regex_and_get_response(pattern: str, string: str):
+    response = 'Текст не найден'
 
-        try:
-            valid_pattern = re.compile(pattern)
-        except re.error:
-            response = 'Регулярное выражение неверно'
-        else:
-            match = valid_pattern.match(string=string)
-
-        if match is not None:
-            response = 'Найдено совпадение'
-
+    try:
+        valid_pattern = re.compile(pattern)
+    except re.error:
+        response = 'Регулярное выражение неверно'
         return response
+    else:
+        match = valid_pattern.match(string=string)
+
+    if match is not None:
+        response = 'Найдено совпадение'
+
+    return response
